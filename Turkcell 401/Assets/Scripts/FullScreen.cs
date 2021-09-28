a134ddfe3f5904da82a94de0675a77dc11efdf8d
denoboi <deniz.kaymakci@hotmail.com>
2021-09-28 16:14:40 +0300

    Screen Adjustment

---
+++ Turkcell 401/Assets/Scripts/FullScreen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreen : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Vector2 tempScale = transform.localScale;
+
+        float spriteWidth = spriteRenderer.size.x;
+        float screenHeight = Camera.main.orthographicSize * 2;
+        float screenWidth = screenHeight / Screen.height * Screen.width;
+
+        tempScale.x = screenWidth / spriteWidth;
+        transform.localScale = tempScale;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
